2128bf8d6b4276b7619ed4361a63a77d2b0d5cfb
Samyocord <samyocord@gmail.com>
2019-02-24 21:43:07 +0100

    Disabled Alt+Esc, Ctrl+Esc, Left and Right Windows Key

--- BluescreenSimulator/Bluescreen.xaml.cs
+++ BluescreenSimulator/Bluescreen.xaml.cs
@@ -15,6 +15,34 @@ namespace BluescreenSimulator
         Thread progressThread = null;
         BluescreenData bluescreenData = null;
 
+        private struct KBDLLHOOKSTRUCT
+        {
+            public int vkCode;
+            int scanCode;
+            public int flags;
+            int time;
+            int dwExtraInfo;
+        }
+
+        private delegate int LowLevelKeyboardProcDelegate(int nCode, int wParam, ref KBDLLHOOKSTRUCT lParam);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProcDelegate lpfn, IntPtr hMod, int dwThreadId);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnhookWindowsHookEx(IntPtr hHook);
+
+        [DllImport("user32.dll")]
+        private static extern int CallNextHookEx(int hHook, int nCode, int wParam, ref KBDLLHOOKSTRUCT lParam);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetModuleHandle(IntPtr path);
+
+        private IntPtr hHook;
+        LowLevelKeyboardProcDelegate hookProc; // prevent gc
+        const int WH_KEYBOARD_LL = 13;
+
+
         public Bluescreen(BluescreenData bluescreenData)
         {
             InitializeComponent();
@@ -28,6 +56,15 @@ namespace BluescreenSimulator
             Closing += Close;
 
             this.KeyDown += Bluescreen_KeyDown;
+
+            IntPtr hModule = GetModuleHandle(IntPtr.Zero);
+            hookProc = new LowLevelKeyboardProcDelegate(LowLevelKeyboardProc);
+            hHook = SetWindowsHookEx(WH_KEYBOARD_LL, hookProc, hModule, 0);
+            if (hHook == IntPtr.Zero)
+            {
+                MessageBox.Show("Failed to set hook, error = " + Marshal.GetLastWin32Error());
+            }
+
         }
 
         void Bluescreen_KeyDown(object sender, KeyEventArgs e)
@@ -176,14 +213,37 @@ namespace BluescreenSimulator
                 case Key.F7:
                     this.Close();
                     break;
-                //Doesn't work
-                case Key.RWin:
-                    e.Handled = true;
-                    break;
-                case Key.LWin:
-                    e.Handled = true;
-                    break;
             }
         }
+
+        private static int LowLevelKeyboardProc(int nCode, int wParam, ref KBDLLHOOKSTRUCT lParam)
+        {
+            if (nCode >= 0)
+                switch (wParam)
+                {
+                    case 256: // WM_KEYDOWN
+                    case 257: // WM_KEYUP
+                    case 260: // WM_SYSKEYDOWN
+                    case 261: // M_SYSKEYUP
+                        if (
+                            (lParam.vkCode == 0x1b && lParam.flags == 32) || // Alt+Esc
+                            (lParam.vkCode == 0x73 && lParam.flags == 32) || // Alt+F4
+                            (lParam.vkCode == 0x1b && lParam.flags == 0) || // Ctrl+Esc
+                            (lParam.vkCode == 0x5b && lParam.flags == 1) || // Left Windows Key 
+                            (lParam.vkCode == 0x5c && lParam.flags == 1))    // Right Windows Key 
+                        {
+                            return 1; //Do not handle key events
+                        }
+                        break;
+                }
+            return CallNextHookEx(0, nCode, wParam, ref lParam);
+        }
+
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            UnhookWindowsHookEx(hHook); // release keyboard hook
+        }
+
     }
 }
