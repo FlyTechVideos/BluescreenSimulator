bc108258105f47152c1e7ec267e83886d89b3ab6
Roco <rococraft7@gmail.com>
2019-02-25 14:04:20 +0200

    Added Windows 7 bsod style

--- BluescreenSimulator/BluescreenWin7.xaml.cs
+++ BluescreenSimulator/BluescreenWin7.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,6 +20,9 @@ namespace BluescreenSimulator
     /// </summary>
     public partial class BluescreenWin7 : Window
     {
+        //Variables
+        private bool DoneDumping = false;
+        Random r_dump = new Random();
         BluescreenWin7Data Win7Data = null;
         public BluescreenWin7(BluescreenWin7Data win7Data)
         {
@@ -29,13 +33,42 @@ namespace BluescreenSimulator
 
         private void SetupScreen()
         {
+            DoneDump1.Visibility = Visibility.Hidden; DoneDump2.Visibility = Visibility.Hidden;
             ErrorCode.Text = Win7Data.ErrorCode;
             Step1.Text = Win7Data.Step1;
             Step2.Text = Win7Data.Step2;
             Tip.Text = Win7Data.Tip;
             StopCode.Text = Win7Data.StopCode;
+            DoneDumping = SimulateDumping();
         }
+        private void AfterDumping()
+        {
+            DoneDump1.Visibility = Visibility.Visible; DoneDump2.Visibility = Visibility.Visible;
 
+        }
+        void DumpLoop()
+        {
+            int MaxDump = 95;
+            var CurrentDump = 0;
+            while (CurrentDump < MaxDump)
+            {
+                Thread.Sleep(r_dump.Next(5000));
+                CurrentDump += r_dump.Next(15, 25);
+                if (CurrentDump > MaxDump)
+                {
+                    CurrentDump = MaxDump;
+                    Application.Current.Dispatcher.InvokeAsync(() => AfterDumping());
+                }
+
+                Application.Current.Dispatcher.InvokeAsync(() => Dump.Text = "Dumping physical memory to disk: " + CurrentDump.ToString());
+            }
+
+        }
+        private bool SimulateDumping()
+        {
+            Task.Factory.StartNew(this.DumpLoop);
+            return true;
+        }
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.F7)
