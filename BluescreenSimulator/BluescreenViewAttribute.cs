922fa0046efda91cdb7bdd7f9c9307fbfa40655a
jeuxjeux20 Y <jeuxjeux20@outlook.fr>
2019-02-28 15:43:45 +0100

    Small refactoring

--- BluescreenSimulator/BluescreenViewAttribute.cs
+++ BluescreenSimulator/BluescreenViewAttribute.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Forms;
 using BluescreenSimulator.ViewModels;
+using BluescreenSimulator.Views;
 using Application = System.Windows.Application;
 
 namespace BluescreenSimulator
@@ -31,6 +33,11 @@ namespace BluescreenSimulator
                 {
                     var window = (Window) Activator.CreateInstance(type, bluescreen);
                     window.ShowOnMonitor(Screen.PrimaryScreen);
+                    foreach (var otherScreen in Screen.AllScreens.Where(s => !Equals(s, Screen.PrimaryScreen)))
+                    {
+                        var blackScreenWindow = new BlackWindow(window);
+                        blackScreenWindow.ShowOnMonitor(otherScreen);
+                    }
                     return window;
                 });
             }
